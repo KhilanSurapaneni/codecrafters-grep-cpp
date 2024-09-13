118116388d107ddcad198879615bbfd19c199d06
KhilanSurapaneni <khilans@gmail.com>
2024-09-13 13:02:35 -0700

    matching a digit

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,17 +2,6 @@
 #include <string>
 #include <algorithm>
 
-bool match_pattern(const std::string& input_line, const std::string& pattern) {
-    if (pattern.length() == 1) {
-        return match_character(input_line, pattern);
-    }
-    else if (pattern == "\\d") {
-        return match_digit(input_line);
-    }
-    else {
-        throw std::runtime_error("Unhandled pattern " + pattern);
-    }
-}
 
 bool match_character(const std::string& input_line, const std::string& pattern){
     return input_line.find(pattern) != std::string::npos;
@@ -25,6 +14,18 @@ bool match_digit(const std::string& input_line) {
         }
     }
     return false;
+}
+
+bool match_pattern(const std::string& input_line, const std::string& pattern) {
+    if (pattern.length() == 1) {
+        return match_character(input_line, pattern);
+    }
+    else if (pattern == "\\d") {
+        return match_digit(input_line);
+    }
+    else {
+        throw std::runtime_error("Unhandled pattern " + pattern);
+    }
 }
 
 int main(int argc, char* argv[]) {
